2c8ee18323fcd009721a7eea0c76e3e36ea55af8
Robert Burnett <dr.slate2@gmail.com>
2023-12-16 12:07:24 -0600

    added another example program to "tests"

--- src/tests.zig
+++ src/tests.zig
@@ -1,7 +1,7 @@
 const testing = @import("std").testing;
 
 test "interface" {
-    testing.refAllDecls(@import("tests/basic.zig"));  
-    testing.refAllDecls(@import("tests/circular_reference.zig"));   
+    testing.refAllDecls(@import("tests/basic.zig"));
+    testing.refAllDecls(@import("tests/circular_reference.zig"));
+    testing.refAllDecls(@import("tests/to_string.zig"));
 }
-
