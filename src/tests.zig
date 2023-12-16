edbe9c0f18babc5849812322cb15d0f20d690fd9
bluesillybeard <bluesillybeard2@gmail.com>
2023-12-16 16:12:17 -0700

    Add to_string_error test

--- src/tests.zig
+++ src/tests.zig
@@ -4,4 +4,8 @@ test "interface" {
     testing.refAllDecls(@import("tests/basic.zig"));
     testing.refAllDecls(@import("tests/circular_reference.zig"));
     testing.refAllDecls(@import("tests/to_string.zig"));
+    
+    // Inferred error sets on interfaces break the compiler.
+    // So, defined error sets are required.
+    testing.refAllDecls(@import("tests/to_string_error.zig"));
 }
---
+++ src/tests/to_string_error.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const testing = std.testing;
+const interface = @import("interface");
+
+//=============Code to create the interface=============
+fn MakePrintable(comptime BaseType: type) type {
+    return struct {
+        // you CANNOT use an inferred error set. It will cause the compiler to crash.
+        pub inline fn to_string(self: BaseType, a: std.mem.Allocator) std.fmt.AllocPrintError![]u8 {
+            return self.vtable.to_string(self.object, a);
+        }
+    };
+}
+// makeInterface creates an interface type that can be used to create implementations
+const Printable = interface.MakeInterface(MakePrintable, .{});
+
+//==============Code to create a type which implements the interface==========
+const SubType = struct {
+    foo: i32,
+    bar: i32,
+
+    pub fn to_string(self: *@This(), a: std.mem.Allocator) std.fmt.AllocPrintError![]u8 {
+        return try std.fmt.allocPrint(a, "[foo: {}, bar: {}]", .{ self.foo, self.bar });
+    }
+};
+
+//==============Code to create a function which accepts the interface type===========
+fn doSomething(a: std.mem.Allocator, printable: Printable) !void {
+    const string = try printable.to_string(a);
+    defer a.free(string);
+}
+
+test "printable interface" {
+    var sub_type_value = SubType{ .foo = 123, .bar = 456 };
+    var sub_type_interface = Printable.initFromImplementer(SubType, &sub_type_value);
+
+    const stringified = try sub_type_interface.to_string(std.testing.allocator);
+    defer std.testing.allocator.free(stringified);
+
+    try doSomething(std.testing.allocator, sub_type_interface);
+    
+}
